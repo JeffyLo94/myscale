6e6c02d4b3f33d007f211e3a49809bc1ad81625a
Lo <jeffrey.lo@Jeffrey-Axonics-Macbook.local>
2022-03-30 15:26:03 -0700

    updated styling and added updated with x0.5,x1, x2, x3 buttons

--- assets/script.js
+++ assets/script.js
@@ -10,19 +10,63 @@ fileUpload.addEventListener("change", function() {
     document.getElementById("detail_width").innerHTML = viewBox.width;
     document.getElementById("detail_height").innerHTML = viewBox.height;
     document.getElementById("svg_details").style.display = "flex";
-    document.getElementById("svg_form").style.display = "flex";
+    document.getElementById("svg_sizing_options").style.display = "flex";
   }
 });
 
-var playGround = function() {
-  var resizeWidthValue = document.getElementById("resize_width_value").value;
-  var resizeHeightValue = document.getElementById("resize_height_value").value;
-  document.getElementsByTagName("svg")[0].setAttribute("id", "svg_resize");
-  document.getElementsByTagName("svg")[0].setAttribute("width", resizeWidthValue);
-  document.getElementsByTagName("svg")[0].setAttribute("height", resizeHeightValue);
+var showCustomResizeForm = function() {
+  document.getElementById("svg_form").style.display = "flex";
+}
+
+var hideCustomResizeForm = function() {
+  document.getElementById("svg_form").style.display = "none";
+}
+
+var showDownloadContainer = function () {
   document.getElementById("download_wrapper").style.display = "block";
-  var fileRename = "icon-" + resizeWidthValue + "x" + resizeHeightValue + "px.png";
-  document.getElementById("data").setAttribute("download", fileRename);
+}
+
+var hideDownloadContainer = function () {
+  document.getElementById("download_wrapper").style.display = "none";
+}
+
+var playGround = function(filename='', multiplier = 1, hasCustomResize=false) {
+  var resizeWidthValue = 0;
+  var resizeHeightValue = 0;
+  var svgWidth = document.getElementsByTagName("svg")[0].getAttribute("width");
+  var svgHeight = document.getElementsByTagName("svg")[0].getAttribute("height");
+  console.log(`orig: ${svgWidth} X ${svgHeight} ${typeof(svgWidth)}`);
+
+  var w = parseInt(svgWidth)
+  var h = parseInt(svgHeight)
+  if( !hasCustomResize ){
+    
+
+    resizeWidthValue = parseInt(svgWidth) * multiplier;
+    resizeHeightValue = parseInt(svgHeight) * multiplier;
+    console.log(`multiplier:\nw: ${resizeWidthValue}\nh: ${resizeHeightValue}`);
+  } else {
+    resizeWidthValue = document.getElementById("resize_width_value").value;
+    resizeHeightValue = document.getElementById("resize_height_value").value;
+    console.log(`resizeWidth: ${resizeWidthValue}\nresizeHeight: ${resizeHeightValue}`);
+  }
+  setPreview( resizeWidthValue, resizeHeightValue, filename );
+}
+
+var setPreview = function( newWidth, newHeight, fileName='' ) {
+  console.log(`setting w${newWidth} h${newHeight}`);
+  document.getElementsByTagName("svg")[0].setAttribute("id", "svg_resize");
+  document.getElementsByTagName("svg")[0].setAttribute("width", newWidth);
+  document.getElementsByTagName("svg")[0].setAttribute("height", newHeight);
+  document.getElementById("detail_width").innerHTML = newWidth;
+  document.getElementById("detail_height").innerHTML = newHeight;
+  if (fileName === '') {
+    var fileRename = "icon-" + newWidth + "x" + newHeight + ".png";
+    document.getElementById("data").setAttribute("download", fileRename);
+  } else {
+    var fileRename = fileName + ".png";
+    document.getElementById("data").setAttribute("download", fileRename);
+  }
 }
 
 // SVG to DATA URL Library
